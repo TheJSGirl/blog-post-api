ecfba5d47778764a7404dbd61258946009bb2fa2
Priyanka Negi <prinegi100@gmail.com>
2017-11-16 14:35:07 +0530

    added one more column in query

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -5,7 +5,7 @@ const {sendResponse} = require('../helpers');
 posts.route('/posts')
   .get(async(req, res) => {
     try{
-      const [data] = await pool.query('SELECT post, userName, createdAt from blogs INNER JOIN users on blogs.id= users.id ');
+      const [data] = await pool.query('SELECT post_title, description, userName, createdAt from blogs INNER JOIN users on blogs.id= users.id ');
       // console.log(data);
       if(data.length === 0){
         return sendResponse(res, 404, [], 'not found');
@@ -17,7 +17,11 @@ posts.route('/posts')
       console.log(err);
       return sendResponse(res, 500, [], 'internal server error');
     }
-  });
+  })
+
+  .post(async (req, res) => {
+    //check the loggedin user
+  })
 
 posts.route('/post/:id')
   .get(async (req, res) => {
@@ -31,35 +35,39 @@ posts.route('/post/:id')
     }
   
     try{
-        // const {id} = req.params;
-        // const [data] = await pool.query(`SELECT b.post, b.createdAt as timeOfPost, u.userName, c.comments, c.createdAt FROM blogs as b INNER JOIN comments as c ON b.id = c.commentedBy inner join users as u ON u.id = c.commentedOn where b.id = ${id}`);
-        const query = `SELECT b.post, b.createdAt as timeOfPost, u.userName
-        FROM 
-        blogs as b 
-        INNER JOIN users as u  
-        ON
-        b.id = u.id
-        where b.id =${id}`;
-
-        const [postDetail] = await pool.query(query);
-
-        if(postDetail.length === 0){
-          return sendResponse(res, 404, [], 'not found');
-        }
-
-        const commentQuery = `SELECT c.comments, c.createdAt, u.userName 
-        FROM comments as c 
-        INNER JOIN 
-        blogs 
-        ON 
-        c.commentedOn = blogs.id 
-        INNER JOIN
-        users as u ON c.commentedBy = u.id
-        where blogs.id =1`;
       
-        const [comments] = await pool.query(commentQuery);
-        
-        postDetail.push(comments);
+      // postDetail query of post
+      const query = `SELECT b.post_title, b.description, b.createdAt as timeOfPost, u.userName
+      FROM 
+      blogs as b 
+      INNER JOIN users as u  
+      ON
+      b.id = u.id
+      where b.id =${id}`;
+
+      //execute query and get postDetail array 
+      const [postDetail] = await pool.query(query);
+
+      if(postDetail.length === 0){
+        return sendResponse(res, 404, [], 'not found');
+      }
+
+      //comments query on post
+      const commentQuery = `SELECT c.comments, c.createdAt, u.userName as commentedBy
+      FROM comments as c 
+      INNER JOIN 
+      blogs 
+      ON 
+      c.commentedOn = blogs.id 
+      INNER JOIN
+      users as u ON c.commentedBy = u.id
+      where blogs.id =${id}`;
+    
+      //execute query and get comments on a particular post
+      const [comments] = await pool.query(commentQuery);
+      
+      postDetail.push(comments);
+
         return sendResponse(res, 200, postDetail, 'successful');
     }
   
