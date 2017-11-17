251f61f9d6cf5523ad97a521179a2f541eb59208
Priyanka Negi <prinegi100@gmail.com>
2017-11-17 12:44:28 +0530

    fix ELINT server.js

--- src/server.js
+++ src/server.js
@@ -4,22 +4,23 @@ const morgan = require('morgan');
 const cors = require('cors');
 const expressValidator = require('express-validator');
 const routes = require('./routes');
+
 const port = process.env.PORT || 3000;
 
-//initiate app
+// initiate app
 const app = express();
 
-//middleware
+// middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
-//route middleware
+// route middleware
 app.use('/api', routes);
 
-//listen port
+// listen port
 app.listen(port, () => {
   console.log('listening on port no.:', port);
 });
