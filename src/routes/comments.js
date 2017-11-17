a90a60c5dc60da89d635b62f068859a6494e3c6c
Priyanka Negi <prinegi100@gmail.com>
2017-11-17 12:39:38 +0530

    fix ELINT  comments.js

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,80 +1,76 @@
 const comments = require('express').Router();
 const pool = require('../db');
-const {sendResponse} = require('../helpers');
+const { sendResponse } = require('../helpers');
 
 comments.route('/:id')
-.post(async (req, res) => {
-  //validate id and comments
-  req.checkBody('id', 'id is missing').exists();
-  req.checkBody('comments', 'comment require').notEmpty().isAlphanumeric();
-  const id = parseInt(req.params.id);
-  const userId = req.user.userId;
-  
-  if(isNaN(id)){
+  .post(async (req, res) => {
+  // validate id and comments
+    req.checkBody('id', 'id is missing').exists();
+    req.checkBody('comments', 'comment require').notEmpty().isAlphanumeric();
+    const id = parseInt(req.params.id, 10);
+    const { userId } = req.user;
+
+    if (isNaN(id)) {
       return sendResponse(res, 422, [], 'invalid parameters');
     }
-  
-  const { comments} = req.body;
-  
-  try{
-    
-    commentData = {
-      comments,
-      commentedBy: userId,
-      commentedOn: id
+
+    const { comments } = req.body;
+
+    try {
+      const commentData = {
+        comments,
+        commentedBy: userId,
+        commentedOn: id,
+      };
+
+      // query to insert comment detail in db
+      const [comment] = await pool.query('INSERT INTO comments SET ?', commentData);
+
+      return sendResponse(res, 200, comment, 'commented on post');
+    } catch (err) {
+      console.error(err);
+      return sendResponse(res, 500, [], 'something went wrong');
     }
-
-    //query to insert comment detail in db
-    const [comment] = await pool.query('INSERT INTO comments SET ?', commentData);
-
-    return sendResponse(res, 200, comment, 'commented on post');
-  }
-
-  catch(err){
-    console.error(err);
-    return sendResponse(res, 500, [], 'something went wrong');
-  }
-})
+  });
 
 comments.route('/:commentId')
-.delete(async(req, res) => {
-  //validate id 
-  req.checkBody('commentId', 'id is missing').notEmpty();
-  const commentId = parseInt(req.params.commentId);
-  const userType = req.user.userType;
-  const logedInUser = req.user.userId;
-  
-  if(isNaN(commentId)){
+  .delete(async (req, res) => {
+  // validate id
+    req.checkBody('commentId', 'id is missing').notEmpty();
+    const commentId = parseInt(req.params.commentId, 10);
+    const { userType } = req.user;
+    const logedInUser = req.user.userId;
+
+    if (isNaN(commentId)) {
       return sendResponse(res, 422, [], 'invalid parameters');
     }
 
-    try{
-      //execute query to find commentedBy user
+    try {
+      // execute query to find commentedBy user
       const [result] = await pool.query(`SELECT commentedBy FROM comments WHERE id = ${commentId}`);
       // console.log(result[0]);
 
-      //validate if result is empty
-      if(result.length === 0){
+      // validate if result is empty
+      if (result.length === 0) {
         return sendResponse(res, 404, [], 'not found');
       }
-      
-      //check if logedInUser is equal to commentedBy then delete
-      if(logedInUser === result[0].commentedBy){
+
+      // check if logedInUser is equal to commentedBy then delete
+      if (logedInUser === result[0].commentedBy) {
         const [deletedData] = await pool.query(`DELETE FROM comments WHERE commentedOn = ${commentId}`);
-        return sendResponse(res, 200, [], 'deletion successful');
+        return sendResponse(res, 200, deletedData, 'deletion successful');
       }
 
-      //check if userType is 1 means admin then delete
-      if(userType  === 1){
+      // check if userType is 1 means admin then delete
+      if (userType === 1) {
         const [deletedData] = await pool.query(`DELETE FROM comments WHERE id= ${commentId}`);
-        return sendResponse(res, 200, [], 'deletion successful');
+        return sendResponse(res, 200, deletedData, 'deletion successful');
       }
       return sendResponse(res, 403, [], 'you are not allowed to perform this action');
-    }
-    catch(err){
+    } catch (err) {
       console.error(err);
       return sendResponse(res, 500, [], 'internal server error');
     }
-})
+  });
 
 module.exports = comments;
