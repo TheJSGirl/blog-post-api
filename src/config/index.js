586de157915bfe96d55dc52fb3e10f2b59f845bb
Priyanka Negi <prinegi100@gmail.com>
2017-11-17 12:41:39 +0530

    fix ELINT login.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,33 +2,31 @@ const env = process.env.NODE_ENV || 'development';
 
 let poolConfig = null;
 
-if(env === 'production'){
+if (env === 'production') {
   poolConfig = {
-    connectionLimit : 5,
+    connectionLimit: 5,
     host: process.env.DB_HOST_NAME,
     user: process.env.DB_USER_NAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  }
+    database: process.env.DB_NAME,
+  };
 }
 
-if(env === 'development'){
+if (env === 'development') {
   poolConfig = {
-    connectionLimit : 5,
-    host            : 'localhost',
-    user            : 'root',
-    password        : 'pinku',
-    database        : 'Blog_Post'
-  }
-}
-
-else if(env === 'test'){
+    connectionLimit: 5,
+    host: 'localhost',
+    user: 'root',
+    password: 'pinku',
+    database: 'Blog_Post',
+  };
+} else if (env === 'test') {
   poolConfig = {
-    connectionLimit : 5,
-    host            : 'localhost',
-    user            : 'root',
-    password        : 'pinku',
-    database        : 'Blog_Post'
-  }
+    connectionLimit: 5,
+    host: 'localhost',
+    user: 'root',
+    password: 'pinku',
+    database: 'Blog_Post',
+  };
 }
 module.exports = poolConfig;
--- src/routes/login.js
+++ src/routes/login.js
@@ -5,41 +5,39 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 login.route('/')
-  .post(async(req, res) => {
-    
-    //validate userName and password
-    req.checkBody('userName', 'userName require').notEmpty().isLength({min:4});
-    req.checkBody('password', 'too short password').notEmpty().isLength({min:5});
+  .post(async (req, res) => {
+    // validate userName and password
+    req.checkBody('userName', 'userName require').notEmpty().isLength({ min: 4 });
+    req.checkBody('password', 'too short password').notEmpty().isLength({ min: 5 });
 
-    let errors = req.validationErrors();
-    
-    if(errors){
-          return sendResponse(res, 422, [], errors[0].msg);
-        }
+    const errors = req.validationErrors();
 
-    //get userName and password from req.body by destructuring
-    const{userName, password} = req.body;
+    if (errors) {
+      return sendResponse(res, 422, [], errors[0].msg);
+    }
 
-    try{
+    // get userName and password from req.body by destructuring
+    const { userName, password } = req.body;
 
+    try {
       const [result] = await pool.query(`SELECT id, password, userType FROM users where userName = '${userName}'`);
 
       const passwordFromDb = result[0].password;
-     
+
       // console.log(passwordFromDb);
       const isValidPassword = await bcrypt.compare(password, passwordFromDb);
 
-      if(!isValidPassword){
+      if (!isValidPassword) {
         return sendResponse(res, 401, [], 'Unauthorised');
       }
 
       const userDetailForToken = {
         userId: result[0].id,
         userType: result[0].userType,
-      }
+      };
 
-      //generate token 
-      const token = jwt.sign(userDetailForToken, 'abcdefghijklmnop', {expiresIn: 60*60});
+      // generate token
+      const token = jwt.sign(userDetailForToken, 'abcdefghijklmnop', { expiresIn: 60 * 60 });
       // console.log('your token is:', token);
 
       // return res.header('x-auth', token).status(200).json({
@@ -47,19 +45,15 @@ login.route('/')
       //   message: 'welcome'
       // });
       res.header('x-auth', token);
-      return sendResponse(res, 200, {token}, 'login successful');
-      
-    }
-    catch(err){
+      return sendResponse(res, 200, { token }, 'login successful');
+    } catch (err) {
       console.error(err);
-      if(err.code === 'ER_BAD_FIELD_ERROR'){
-        return sendResponse(res, 400, [], 'bad request' );
+      if (err.code === 'ER_BAD_FIELD_ERROR') {
+        return sendResponse(res, 400, [], 'bad request');
       }
-
       return sendResponse(res, 500, [], 'something went wrong');
     }
-    
-  })
+  });
 
 
 module.exports = login;
