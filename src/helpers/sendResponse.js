31cdfaaa230e95b1a14387f6c3039a479c2ed65c
Priyanka Negi <prinegi100@gmail.com>
2017-11-17 16:50:14 +0530

    modified sendResponse()

--- src/helpers/sendResponse.js
+++ src/helpers/sendResponse.js
@@ -1,6 +1,14 @@
-const sendResponse = (res, statusCode, data, message) => res.status(statusCode).json({
-  data,
-  message,
-});
+const sendResponse = (res, statusCode, data, message) => {
+  const status = null;
+  const pattern = /^2\d{2}$/;
+  // if statusCode starts with 2, set status values as sucess else fail
+  pattern.test(statusCode) ? status = 'success' : status = 'failure';
+
+  res.status(statusCode).json({
+    data,
+    status,
+    message,
+  });
+};
 
 module.exports = sendResponse;
