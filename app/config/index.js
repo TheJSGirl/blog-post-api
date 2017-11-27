f90f3adf90d1faabe43d826fa60f9134c2d10fbc
Priyanka Negi <prinegi100@gmail.com>
2017-11-27 19:55:41 +0530

    changed token expiry time

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 process.env.JWT_SECRET = 'thisisasimplesecreat';
-process.env.JWT_EXPIRY = ((1 * 24) * 60) * 60;
+process.env.JWT_EXPIRY = '1d';
 
 let poolConfig = null;
 if (env === 'production') {
