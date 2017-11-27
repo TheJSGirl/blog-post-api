08fc20e01f5d1a41eb90df8850042079e96c3753
Priyanka Negi <prinegi100@gmail.com>
2017-11-27 14:26:49 +0530

    update error message

--- app/routes/login.js
+++ app/routes/login.js
@@ -22,7 +22,7 @@ login.route('/')
       const [result] = await pool.query(`SELECT id, password, userType FROM users where username = '${username}'`);
 
       if (result.length === 0) {
-        return sendResponse(res, 404, [], 'user not found');
+        return sendResponse(res, 404, [], 'Username not registered');
       }
       const passwordFromDb = result[0].password;
 
@@ -30,7 +30,7 @@ login.route('/')
       const isValidPassword = await bcrypt.compare(password, passwordFromDb);
 
       if (!isValidPassword) {
-        return sendResponse(res, 401, [], 'Unauthorised');
+        return sendResponse(res, 401, [], 'Failed to authenticate');
       }
 
       const userDetailForToken = {
@@ -45,12 +45,9 @@ login.route('/')
       // set token in response header
       res.header('x-auth', token);
       // also send token in response body
-      return sendResponse(res, 200, { token }, 'login successful');
+      return sendResponse(res, 200, { token }, 'Login successful');
     } catch (err) {
       console.error(err);
-      if (err.code === 'ER_BAD_FIELD_ERROR') {
-        return sendResponse(res, 400, [], 'bad request');
-      }
       return sendResponse(res, 500, [], 'something went wrong');
     }
   });
